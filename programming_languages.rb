849d33325911a5faa86df2e467cd293f1a11f0ce
Stephen F <stephen@farver.io>
2018-05-31 22:24:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |i_style, i_language_names|
+    i_language_names do |i_language_name, i_type|
+      if !new_hash[i_language_name]
+        
+        new_hash[i_language_name] = 
+        {
+          type: ,
+          style: []
+        }
+      
+      
+    end
+  end
+  
+  new_hash
 end
